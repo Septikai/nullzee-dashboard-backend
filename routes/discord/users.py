359c72e448fc7207509948d1efff2b4f493fc85a
Kian Spence <septikaiwolf@gmail.com>
2022-04-30 23:06:35 +0100

    punishments update

--- routes/discord/users.py
+++ routes/discord/users.py
@@ -1,11 +1,15 @@
+import pymongo
 from flask import Flask
-import requests
+import requests, time
 
 from helpers import res, cors
 import runtime_config
 from utils.constants import DISCORD_API_URL
 from utils.json_wrapper import JsonWrapper
 
+
+# TODO: replace the dict below with a json file
+mods_fetched = {}
 
 def auth_request(endpoint, access_token) -> JsonWrapper or list:
     headers = {
@@ -75,8 +79,27 @@ def setup(app: Flask):
             return res.json(code=404)
 
         # TODO: implement a check for if the user does not have a db entry
-        user_coll_entry = list(runtime_config.mongodb_moderation_collection.find({"offender_id": int(user_id)}))
+        user_coll_entry = list(runtime_config.mongodb_moderation_collection.find({"offender_id": int(user_id)}, sort=[("_id", pymongo.DESCENDING)]))
         user_coll_entry = [{key: value for key, value in d.items() if key != "_id"} for d in user_coll_entry]
+        for d in user_coll_entry:
+            d.update((k, str(v)) for k, v in d.items() if k == "mod_id")
+
+        for punishment in user_coll_entry:
+            if punishment["mod_id"] in mods_fetched and time.time() - punishment["mod_id"]["fetched_at"] < 86400:
+                punishment["mod_pfp"] = mods_fetched[punishment["mod_id"]]["avatar"]
+                punishment["mod_username"] = mods_fetched[punishment["mod_id"]]["username"]
+            else:
+                mod = get_guild_member(punishment["mod_id"])
+                try:
+                    punishment["mod_pfp"] = mod["user"]["avatar"]
+                    punishment["mod_username"] = f"{mod['user']['username']}#{mod['user']['discriminator']}"
+                    mods_fetched[punishment["mod_id"]] = {}
+                    mods_fetched[punishment["mod_id"]]["avatar"] = mod["user"]["avatar"]
+                    mods_fetched[punishment["mod_id"]]["username"] = f"{mod['user']['username']}#{mod['user']['discriminator']}"
+                    mods_fetched[punishment["mod_id"]]["fetched_at"] = time.time()
+                except KeyError:
+                    print(mod)
+
         print(f"punishments - {user_coll_entry}")
 
         return res.json({
